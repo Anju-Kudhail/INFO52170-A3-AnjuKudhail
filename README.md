87b058d57056bc8be3e9a8564414b1fc27c194a0
Anju-Kudhail <Anjunayyar.pm@gmail.com>
2026-07-08 14:39:55 -0400

    Initial commit - Anju Kudhail

---
+++ README.md
@@ -0,0 +1,2 @@
+# INFO52170 Assessment 3 - Anju Kudhail
+GitHub Actions CI assessment for INFO 52170.
---
+++ app.py
@@ -0,0 +1,6 @@
+# app.py - Created by Firstname Lastname
+def greet(name):
+    return f"Hello, {name}! Welcome to INFO 52170."
+
+if __name__ == "__main__":
+    print(greet("Anju"))
